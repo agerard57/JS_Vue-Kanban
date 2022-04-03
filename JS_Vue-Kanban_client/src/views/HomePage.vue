7a7f1072671d2f7387f13a05ce83cece14d5fdb3
GERARD Alexandre <agerard57@protonmail.com>
2022-04-03 21:30:58 +0200

    feat: Add home page

---
+++ JS_Vue-Kanban_client/src/views/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="content text-center">
+    <p>This is the home page</p>
+    <p>3 Last tasks...</p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
