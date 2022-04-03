e084e3e33ca2ec9a1140d499aa7c9df1423edec7
GERARD Alexandre <agerard57@protonmail.com>
2022-04-03 21:30:42 +0200

    feat: Add missing page

---
+++ JS_Vue-Kanban_client/src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="content text-center">
+    <p>Oops, this page doesn't exist, the sadness...</p>
+    <router-link :to="{ name: 'Home page' }">Back to the home page</router-link>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
