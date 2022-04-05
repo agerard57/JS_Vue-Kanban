2b1d7c183adfe66bfef6d96fbb25daaab471a098
agerard57 <agerard57@protonmail.com>
2022-04-05 18:03:20 +0200

    refacto: Task form

---
+++ JS_Vue-Kanban_client/src/views/TaskFormPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <task-form list="list" id="id" />
+</template>
+
+<script>
+import TaskForm from "../components/taskFormPage/TaskForm";
+
+export default {
+  name: "TaskFormPage",
+  components: {
+    TaskForm,
+  },
+  props: ["id", "list"],
+};
+</script>
