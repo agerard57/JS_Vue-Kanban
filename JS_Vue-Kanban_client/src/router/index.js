dc18c6f4530225f968792255e2d47d890ac008fd
agerard57 <agerard57@protonmail.com>
2022-04-05 17:42:56 +0200

    refacto: Move router to it's own package

---
+++ JS_Vue-Kanban_client/src/router/index.js
@@ -0,0 +1,65 @@
+import Vue from "vue";
+import Router from "vue-router";
+import HomePage from "../views/HomePage.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history", // Deactivates the "hash routes" mode (otherwise paths would be /#/page)
+  linkExactActiveClass: "active",
+
+  routes: [
+    {
+      path: "/",
+      name: "Home page",
+      component: HomePage,
+      meta: {
+        title: "Home Page - Kanban",
+        metaTags: [
+          {
+            name: "description",
+            content: "The home page of the kanban app.",
+          },
+          {
+            property: "og:description",
+            content: "The home page of the kanban app.",
+          },
+        ],
+      },
+    },
+    {
+      path: "/todos",
+      name: "Tasks lists",
+      component: () => import("../views/TodoList.vue"),
+    },
+    {
+      path: "/add",
+      props: true,
+      name: "Add a new task",
+      component: () => import("../views/TaskFormPage.vue"),
+    },
+    {
+      path: "/task/:id",
+      props: true,
+      name: "View task",
+      component: () => import("../views/TaskFormPage.vue"),
+    },
+    {
+      path: "/edit/:id",
+      props: true,
+      name: "Edit task",
+      component: () => import("../views/TaskFormPage.vue"),
+    },
+    {
+      path: "/user/auth",
+      props: true,
+      name: "Sign up / Login",
+      component: () => import("../views/AuthFormPage.vue"),
+    },
+    {
+      path: "*",
+      name: "Page not found",
+      component: () => import("../views/NotFound.vue"),
+    },
+  ],
+});
