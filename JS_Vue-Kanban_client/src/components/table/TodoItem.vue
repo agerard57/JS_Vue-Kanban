465e6af591ed83ebcb039dc79a6bacddc4e27b19
agerard57 <agerard57@protonmail.com>
2022-04-05 14:19:57 +0200

    feat: Add buttons will not display if in home

--- JS_Vue-Kanban_client/src/components/table/TodoItem.vue
+++ JS_Vue-Kanban_client/src/components/table/TodoItem.vue
@@ -17,7 +17,7 @@
           <card-buttons v-bind:id="todo.id" />
         </div>
       </div>
-      <div class="card-body p-3" v-if="index == lastTodo">
+      <div class="card-body p-3" v-if="index == lastTodo && !isHomePage">
         <router-link
           class="btn btn-primary btn-block"
           :to="{ name: 'Add a new task', params: { list: todo.list } }"
@@ -41,17 +41,21 @@ export default {
     error: "",
     todos: [],
   }),
-
   mounted() {
-    axios
-      .get("http://localhost:3000/todos")
-      .then((response) => (this.todos = response.data))
-      .then((results) => {
-        const filteredResults = results.filter(
-          (element) => element.list === this.columnTitle
+    axios.get("http://localhost:3000/todos").then((results) => {
+      let filteredResults = results.data.filter(
+        (element) => element.list === this.columnTitle
+      );
+
+      if (this.columnTitle === "FAVOURITES")
+        filteredResults = results.data.filter(
+          (element) => element.fav === true
         );
-        this.todos = filteredResults;
-      });
+      else if (this.columnTitle === "LATEST TASKS")
+        filteredResults = results.data.sort((x, y) => y.id - x.id).splice(0, 3);
+
+      this.todos = filteredResults;
+    });
   },
   computed: {
     lastTodo() {
@@ -59,6 +63,9 @@ export default {
         (Array.isArray(this.todos) ? this.todos : Object.keys(this.todos))
           .length - 1
       );
+    },
+    isHomePage() {
+      return this.$route.name === "Home page";
     },
   },
 };
