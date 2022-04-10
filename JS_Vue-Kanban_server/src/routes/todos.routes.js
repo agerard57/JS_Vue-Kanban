ea3e903b0b37a92d8585e3e0ea1c766f0454782b
GERARD Alexandre <agerard57@protonmail.com>
2022-04-10 05:16:31 +0200

    feat: Secure routes with authentification

--- JS_Vue-Kanban_server/src/routes/todos.routes.js
+++ JS_Vue-Kanban_server/src/routes/todos.routes.js
@@ -1,16 +1,14 @@
 const todosController = require("../controllers/todos.controller");
 const { authJwt } = require("../middlewares");
+
 module.exports = function (app) {
   // GET all todos / POST new todo
-  app
-    .route("/todos/")
-    .get([authJwt], todosController.getAll)
-    .post(todosController.add);
+  app.route("/todos/").get(todosController.getAll).post(todosController.add);
 
   // GET todo by id / PUT to update a todo by id / DELETE todo by id
   app
     .route("/todos/:id")
     .get(todosController.getOne)
-    .put(todosController.update)
-    .delete(todosController.delete);
+    .put([authJwt], todosController.update)
+    .delete([authJwt], todosController.delete);
 };
